2f12d1eaf7fad31d30809d645f2d86ee7711540c
penmerchant <aniqnoob666@gmail.com>
2023-01-11 14:04:08 +0800

    added bookmark button on postDetails module

---
+++ client/src/src/pages/post/components/PostReactions/BookmarkReactions.js
@@ -0,0 +1,20 @@
+import {BsBookmark, BsBookmarkFill} from 'react-icons/bs';
+import classes from '../../PostDetails.module.css';
+
+const BookmarkReactions = (props) =>{
+    const {isBookmarked,
+        handleReactions,
+        action,
+        actionId,
+    } = props;
+    const effect = isBookmarked? 'unBookmark': 'bookmark';
+
+    const handleAction = () => {
+        handleReactions(effect, action, actionId, 'isBookmarked');
+    };
+    
+    return (<div className={classes.icon_wrapper} onClick={handleAction}>
+        {isBookmarked? <BsBookmarkFill  />: <BsBookmark />}
+    </div>);
+};
+export default BookmarkReactions;
--- client/src/src/pages/post/components/PostReactions/PostReaction.js
+++ client/src/src/pages/post/components/PostReactions/PostReaction.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import AuthContext from "../../../../context/authContext";
 import usePostReactions from "../../../../hooks/usePostReactions";
-import BookmarkReactions from "./BookmarkReactionsButton";
+import BookmarkReactions from "./BookmarkReactions";
 import LikeReactionButton from "./LikeReactionButton";
 
 const PostReactions = ({likes, bookmarked, postId}) => {

69734ed359fa7460f97c6ea1aaeb6f5bfa37924d
penmerchant <aniqnoob666@gmail.com>
2023-03-05 17:58:10 +0800

    finished working on sorting by tags input

--- client/src/src/pages/ResultPage/ReadingList.js
+++ client/src/src/pages/ResultPage/ReadingList.js
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import DropDown from '../../components/FormElements/Dropdown/Dropdown';
 import PostCards from '../../components/list/components/PostCards'
 import HomeSkeleton from '../../components/Skeleton/HomeSkeleton';
@@ -8,12 +8,13 @@ import classes from './page.module.css';
 import Button from '../../components/Button/Button';
 const ReadingList = () => {
     const [post, setSavedPosts] = useState([]);
-    const [tags, setTags] = useState([]);
+    const [allPost , setAllPost] = useState([]);
+    const [tags, settags] = useState([]);
     const [chosenItem, setChosen] = useState('');
     const {isLoggedin, currentUser} = useContext(AuthContext);
     const {sendRequest, isLoading, isError} = useHttp();
 
-    useEffect(()=>{
+    useMemo(()=>{
         // if(!isLoggedin){
 
         // }
@@ -21,35 +22,48 @@ const ReadingList = () => {
             try {
                 const response = await sendRequest(`${process.env.REACT_APP_API_URL}/api/user/saved-posts/${currentUser.data.id}`);
                 setSavedPosts(response.savedPost);
-                setTags(response.followedTags);
+                settags(response.followedTags);
+                setAllPost(response.savedPost);
             } catch (error){
 
             }
         };
 
         fetchSavedPosts();
-    },[setSavedPosts,currentUser,sendRequest]);
+        // setAllPost(post);
+    },[setSavedPosts,currentUser,sendRequest,settags,setAllPost]);
 
     const onChange = useCallback((item)=>{
         setChosen(item);
     },[setChosen]);
 
-    const handleChosenItem = () => {
-        console.log(chosenItem);
+    // const handleChosenItem = useMemo(()=>{
+    //     setSavedPosts();
+    // },[setSavedPosts]);
+
+    const fetchSortedPost = () =>{
+        const sortedTagsPost = allPost.filter(found=> {
+            return found.tags.find(elem => elem.name === chosenItem);
+        }); 
+        setSavedPosts(sortedTagsPost);
     };
+
     if(isLoading){
         return <HomeSkeleton />;
+    }
+    if(isError) {
+        return <div>An error occurred </div>
     }
     return <div className={classes.grid_display}>
         <div className={classes.row}>
             <DropDown type='tags' items={tags} onChange={onChange}/>
-            <Button label='Choose' onClick={()=> handleChosenItem}/>
+            <Button label='Choose' onClick={fetchSortedPost}/>
         </div>
         <div >
             {
-                post && post.map((ps)=>{
+                post? post.map((ps)=>{
                     return <PostCards post={ps} user={ps.author}/>
-                })
+                }) : 'Found no result'
             }
         </div>
     </div>

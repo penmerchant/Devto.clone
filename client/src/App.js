4a82f1e628a796f0009b6a7a887a148a36a46d09
penmerchant <aniqnoob666@gmail.com>
2023-03-08 19:51:36 +0800

    restructure file hierarchy of client-side

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "./src/components/Layout/Layout";
-import NewPost from "./src/pages/NewPost/createPost";
-import Home from "./src/pages/home/home";
-import Login from "./src/pages/login";
-import CreateAccount from "./src/pages/authentication/register";
-import SearchPage from "./src/pages/ResultPage/SearchPage";
-import PostDetails from "./src/pages/post/postDetails";
-import Footer from "./src/components/Footer/Footer";
-import About from "./src/pages/about/about";
-import Profile from "./src/pages/profile/profile";
-import EditPost from "./src/pages/edit/editpost/EditPost";
-import EditProfile from "./src/pages/edit/editprofile/EditProfile";
-import TagsView from "./src/pages/Tags/Tags";
-import DeletePage from "./src/components/DeleteBox/DeletePage";
-import EditComment from "./src/pages/edit/editcomment/EditComment";
-import ReadingList from "./src/pages/ResultPage/ReadingList";
-import TagsResultPage from "./src/pages/ResultPage/TagsResultList";
+// import Layout from "./src/components/Layout/Layout";
+import Layout from "../src/components/Layout/Layout";
+import NewPost from "../src/pages/NewPost/createPost";
+import Home from "../src/pages/home/home";
+import Login from "../src/pages/login";
+import CreateAccount from "../src/pages/authentication/register";
+import SearchPage from "../src/pages/ResultPage/SearchPage";
+import PostDetails from "../src/pages/post/postDetails";
+import Footer from "../src/components/Footer/Footer";
+import About from "../src/pages/about/about";
+import Profile from "../src/pages/profile/profile";
+import EditPost from "../src/pages/edit/editpost/EditPost";
+import EditProfile from "../src/pages/edit/editprofile/EditProfile";
+import TagsView from "../src/pages/Tags/Tags";
+import DeletePage from "../src/components/DeleteBox/DeletePage";
+import EditComment from "../src/pages/edit/editcomment/EditComment";
+import ReadingList from "../src/pages/ResultPage/ReadingList";
+import TagsResultPage from "../src/pages/ResultPage/TagsResultList";
 function App() {
   return (
     <>
     <Layout>
       <Routes>
-        <Route path= '/' element={<Home/>}/>
+        <Route path= '/' element={<Home />}/>
         <Route path='/newPost' element={<NewPost />}/>
-        <Route path='/profile/:userId' element={<Profile/>}/>
+        <Route path='/profile/:userId' element={<Profile />}/>
         <Route path='/login' element={<Login />}/>
         <Route path='/searchResult/:searchValue' element={<SearchPage />} />
         <Route path='/register' element={<CreateAccount />}/>
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import UserProvider from './src/provider/userProvider';
+import UserProvider from '../src/provider/userProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
